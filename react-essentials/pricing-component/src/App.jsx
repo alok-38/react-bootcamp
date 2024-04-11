52cbf82bc3d2598d0661af3ed1881db2df1efaad
Alokananda Y <plantsfortrees@gmail.com>
2024-04-11 13:33:28 +0530

    banner

--- react-essentials/pricing-component/src/App.jsx
+++ react-essentials/pricing-component/src/App.jsx
@@ -1,12 +1,13 @@
-import Pricing from "./components/Pricing"
+import Pricing from "./components/Pricing";
 
 function App() {
-
   return (
-      <div>
+    <div className="flex justify-center items-center h-screen ">
+      <div className="max-w-prose bg-orange-300 p-28 leading-10">
         <Pricing />
       </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
