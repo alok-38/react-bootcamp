6c6917799840167bbe04b1f62f889d64ee0cd792
Alokananda Y <plantsfortrees@gmail.com>
2024-04-11 09:55:37 +0530

    fucn

---
+++ javascript-to-react/passing-arguments/App.js
@@ -0,0 +1,6 @@
+import Header from './components/Header';
+import Main from './components/Main';
+
+const app = document.getElementById('app');
+app.appendChild(Header());
+app.appendChild(Main());
---
+++ javascript-to-react/passing-arguments/components/Header.js
@@ -0,0 +1,7 @@
+const Header = () => {
+    const header = document.createElement('h1');
+    header.textContent = '👾 My Computer Setup';
+    return header;
+};
+
+export default Header;
