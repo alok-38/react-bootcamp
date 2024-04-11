f79495fcab943d2bf56978e2617612071ba89b9e
Alokananda Y <plantsfortrees@gmail.com>
2024-04-11 09:35:49 +0530

    functions

--- javascript-to-react/function-composition/app.js
+++ javascript-to-react/function-composition/app.js
@@ -1,21 +1,21 @@
+const videoSrc = 'https://reactbootcamp.nyc3.digitaloceanspaces.com/big-buck-bunny.mp4';
+
 function createButton(action, style) {
-    return `<button style="${style}">${action} Video</button>`;
+  return `<button style="${style}">${action} Video</button>`;
 }
 
 function createHeader(title, style) {
-    return `<h1 style="${style}">${title}</h1>`;
+  return `<h1 style="${style}">${title}</h1>`;
 }
 
-const videoSrc = "https://reactbootcamp.nyc3.digitaloceanspaces.com/big-buck-bunny.mp4";
-
-function createVideo() {
-    return `<video controls src="${videoSrc}"></video>`;
+function createVideo(src) {
+  return `<video src="${src}" controls style="width: 100%; height: 300px;"></video>`;
 }
 
 const app = document.getElementById('app');
 app.innerHTML = `
-    ${createButton("Like", "background-color: green;")}
-    ${createButton("Dislike", "background-color: red;")}
-    ${createHeader("Watch", "color: blue;")}
-    ${createVideo()}
+${createHeader('Watch Big Buck Bunny', 'color: blue;')}
+${createVideo(videoSrc)}
+${createButton('Like', 'background-color: green;')}
+${createButton('Dislike', 'background-color: red;')}
 `;
