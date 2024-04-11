7731fea8636a30cf7169dd496205b50a9ce4fb68
Alokananda Y <plantsfortrees@gmail.com>
2024-04-11 12:47:02 +0530

    pricing

---
+++ react-essentials/pricing-component/src/components/Pricing.jsx
@@ -0,0 +1,10 @@
+export default function Pricing() {
+    return (
+        <>
+            <h2>Simple no tricks pricing</h2>
+            <a href="/buy" className="button">
+                Buy Now <span aria-hidden="true">→</span>
+            </a>
+        </>
+    );
+}
