5d300287d6dd273ed2da9ddae2935acda5a98850
Alokananda Y <plantsfortrees@gmail.com>
2024-04-25 13:49:49 +0530

    HTML to JSX

--- react-essentials/html-jsx/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header({ text = "HTML to JSX", classes = "" }) {
-	return (
-		<h1 className={`text-5xl font-bold underline text-center mt-20 ${classes}`}>
-			{text}
-		</h1>
-	);
-}
