6890e021d319b19bd223d05edc56e74f7b749b3d
Alokananda Y <plantsfortrees@gmail.com>
2024-05-11 18:00:06 +0530

    Conditional Styles

--- javaScript-to-react/conditional-styles/app.js
+++ javaScript-to-react/conditional-styles/app.js
@@ -1,12 +1,18 @@
-function DownloadButton() {
+function DownloadButton(props) {
+  const { isPaid } = props;
+
+  // Using ternary operators to conditionally set the buttonText and buttonClass
+  const buttonText = isPaid ? "Buy Now" : "Download Free";
+  const buttonClass = isPaid ? "download-button paid" : "download-button";
+
   return `
-	  <button class="download-button">
-		<span>Download Free</span>
+	  <button class="${buttonClass}">
+		<span>${buttonText}</span>
 	  </button>
 	`;
 }
 
 const appDiv = document.getElementById("app");
 appDiv.innerHTML = DownloadButton({
-  isPaid: true,
+  isPaid: false, // or false based on your requirement
 });
